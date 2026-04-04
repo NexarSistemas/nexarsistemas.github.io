aadd5aea695348b000549153ae5c7e371dced547
Rolo JN Beltrán <mailxsuscripcion@gmail.com>
2026-04-04 13:30:44 -0300

    Actualizar main.js

--- js/main.js
+++ js/main.js
@@ -85,3 +85,20 @@ Gracias.`
 // ==============================
 
 console.log("Nexar Web cargada correctamente");
+
+// ==============================
+// Animación simple (segura)
+// ==============================
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach((card, index) => {
+        setTimeout(() => {
+            card.style.opacity = "1";
+            card.style.transform = "translateY(0)";
+        }, index * 100);
+    });
+
+});
