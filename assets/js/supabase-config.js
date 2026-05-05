aa22c352fd03f185dae4beaccc1b5bfec92e1f5b
rolojnb <mailxsuscripcion@gmail.com>
2026-05-05 13:42:41 -0300

    configura supabase con clave anon

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -1,4 +1,4 @@
 window.NEXAR_SUPABASE_CONFIG = {
-  url: "https://TU-PROYECTO.supabase.co",
-  anonKey: "TU_ANON_PUBLIC_KEY"
+  url: "https://qwlngclrhpezelqddlsp.supabase.co/rest/v1/",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3bG5nY2xyaHBlemVscWRkbHNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYyNjU1NjIsImV4cCI6MjA5MTg0MTU2Mn0.djYMUveL-M0uSj07IgkVdLakziG1GMMxn06Y0tqFSmg"
 };
